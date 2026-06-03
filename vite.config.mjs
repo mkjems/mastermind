39fc09822e2c455532f2ee060b5d8fe9d9d0da8f
Martin Kjems <mkjems@gmail.com>
2026-06-03 14:10:49 +0200

    move index.html to repo root

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-  root: 'src',
-  build: {
-    outDir: '../build',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        index: path.resolve(__dirname, 'src/index.html')
-      }
-    }
-  },
   server: {
-    port: 5173
+    port: 3000
   }
 });
