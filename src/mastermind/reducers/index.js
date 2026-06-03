a5d0060121380df96496e13771b75e4ca6a26311
Martin Kjems <mkjems@gmail.com>
2026-06-03 14:29:12 +0200

    Combine tiny reducer files

--- src/mastermind/reducers/index.js
+++ src/mastermind/reducers/index.js
@@ -1,14 +1,103 @@
 
-import activeRowReducer from './activeRow';
-import rowReducer from './row';
 import boardReducer from './board';
 import secretCodeReducer from './secretCode';
-import showColorsReducer from './showColors.js';
-import selectedPegReducer from './selectedPeg.js';
-import isCodeHiddenReducer from './isCodeHidden';
-import gameStatusReducer from './gameStatus';
-import isRulesHiddenReducer from './isRulesHidden';
-import isRevealHiddenReducer from './isRevealHidden';
+
+const isRevealHiddenReducer = (state=false, action)=>{
+	switch (action.type) {
+		case 'GAME_GIVE_UP':
+			return true;
+		case 'GAME_BEGIN':
+			return false;
+		case 'GAME_WIN':
+			return true;
+		case 'RESET_GAME':
+			return false;
+		default:
+			return state
+	}
+};
+
+const isRulesHiddenReducer = (state=true, action)=>{
+	switch (action.type) {
+		case 'TOGGLE_RULES':
+			return !state;
+		default:
+			return state
+	}
+};
+
+const gameStatusReducer = (state='intro', action)=>{
+	switch (action.type) {
+		case 'GAME_BEGIN':
+			return 'playing';
+		case 'GAME_WIN':
+			return 'won';
+		case 'GAME_LOSE':
+			return 'lost';
+		case 'GAME_GIVE_UP':
+			return 'gave_up';
+		case 'GAME_INTRO':
+			return 'intro';
+		default:
+			return state
+	}
+};
+
+const isCodeHiddenReducer = (state=true, action)=>{
+	switch (action.type) {
+		case 'HIDE_SECRET_CODE':
+			return true;
+		case 'REVEAL_SECRET_CODE':
+			return false;
+		case 'TOGGLE_SECRET_CODE':
+			return !state;
+		case 'RESET_GAME':
+			return true;
+		default:
+			return state
+	}
+};
+
+const selectedPegReducer = (state=undefined, action) => {
+	switch (action.type) {
+		case 'SHOW_COLOR_PICKER':
+			return action.id;
+		case 'BEGIN_NEW_ROW':
+		case 'RESET_GAME':
+		case 'ENTER_ROW':
+			return undefined;
+		case 'ADVANCE_SELECTOR': {
+			const selectedPeg = action.pegs.findIndex((peg) => peg === 'select');
+			return selectedPeg === -1 ? undefined : selectedPeg;
+		}
+		default:
+			return state
+	}
+};
+
+const activeRowReducer = (state=0, action)=>{
+	switch (action.type) {
+		case 'BEGIN_NEW_ROW':
+			return state + 1;
+		case 'RESET_GAME':
+			return 0;
+		default:
+			return state
+	}
+};
+
+const showColorsReducer = (state=false, action) => {
+	switch (action.type) {
+		case 'SHOW_COLOR_PICKER':
+			return true;
+		case 'HIDE_COLOR_PICKER':
+			return false;
+		case 'RESET_GAME':
+			return false;
+		default:
+			return state
+	}
+};
 
 const reducer = (state = {}, action) => {
 	const isRevealHidden = isRevealHiddenReducer(state.isRevealHidden, action);
